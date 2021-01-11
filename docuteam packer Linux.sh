e6c8a06f1bfd8a0b024fab8af731679690013ad0
wildit <t.wildi@docuteam.ch>
2021-01-11 16:58:54 +0100

    changed properties

--- docuteam packer Linux.sh
+++ docuteam packer Linux.sh
@@ -4,10 +4,4 @@
 export	LC_CTYPE="UTF-8"
 
 cd		"$(dirname "$0")/docuteam packer.app/Contents/docuteam packer"
-java	-Xms512M -Xmx2048M -Dfile.encoding=utf-8 -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -cp config/:./:docuteam-packer-5.4.0.jar ch.docuteam.packer.gui.launcher.LauncherView &
-
-#		Additional optional parameters:
-
-#		-configDir="/Users/denis/Documents/Java/Workspace/Docupack/config"
-#		-open="/Users/denis/Documents/Java/Workspace/Docupack/files/DifferentFileTypes"
-#		-help
+java	 -Xms512M -Xmx2048M -Dfile.encoding=utf-8 -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -cp config/:./:docuteam-packer-5.4.0.jar ch.docuteam.packer.gui.launcher.LauncherView &
